677f8a53f71d859dc029a43cfa0a74bfacc4d1fb
Erika Matesz Bueno <mateszbueno@gmail.com>
2022-03-29 22:46:10 -0300

    Initial Commit

---
+++ HowIsTheWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HowIsTheWeather
+//
+//  Created by Erika Cristina Bueno on 29/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
